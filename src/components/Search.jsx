5ed46c07a64121072a7f425299159dbd0b4bb408
rubenaguilardev <ragui56@wgu.edu>
2025-05-05 22:48:23 -0700

    add header, add search form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,8 @@
 export default function Search() {
     return (
         <form>
-            <input type="search" placeholder="e.g. oregano"/>
-            <button type="submit">+ Add ingredient</button>
+            <input type="search" placeholder="e.g. oregano" aria-label="add"/>
+            <button type="submit">Add ingredient</button>
         </form>
     )
 }
