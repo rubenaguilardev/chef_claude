2ec41add0ce33b5dd8feae48582066fda5cd394f
rubenaguilardev <ragui56@wgu.edu>
2025-05-05 22:31:57 -0700

    add header, add search form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import Search from './components/Search'
+
+export default function App() {
+    return (
+        <main>
+            <Header/>
+            <Search/>
+        </main>
+        
+    )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+    return (
+        <form>
+            <input type="search" placeholder="e.g. oregano"/>
+            <button type="submit">+ Add ingredient</button>
+        </form>
+    )
+}
