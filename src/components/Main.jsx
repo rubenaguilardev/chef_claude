915d5472bc8da656262631bf1a0e88089c379a8a
rubenaguilardev <ragui56@wgu.edu>
2025-05-09 01:31:24 -0700

    add section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,16 +16,32 @@ export default function Main() {
         <main>
             <form action={addItem}>
                 <input 
-                    type="s"
+                    type="search"
                     name="ingredient" 
                     placeholder="e.g. oregano" 
                     aria-label="add ingredient"
                 />
                 <button type="submit">Add ingredient</button>
-            </form>
-            <ul>
-                {listItems}
-            </ul>
+            </form>   
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul>
+                    {listItems}
+                </ul>
+                <div className="getRecipe">
+                <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section> 
         </main>
+        
     )
 }
+
+
+{/* <ul>
+                {listItems}
+            </ul> */}
